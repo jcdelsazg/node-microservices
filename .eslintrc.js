c61a7743f46bf52e50cbe7a59ed353092733854c
Juan Carlos Del Saz <jcdelsazg@gmail.com>
2020-01-17 16:55:15 +0100

    adding configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'linebreak-style': 0,
+    'comma-dangle': ['error', 'only-multiline'],
+    'arrow-body-style': ['error', 'as-needed']
+  }
+};
