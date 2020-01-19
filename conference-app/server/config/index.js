bf335ec1a396b8edece816e5c5c1cc8575bdd714
Juan Carlos Del Saz <jcdelsazg@gmail.com>
2020-01-19 21:04:19 +0100

    cleaning up main app and using the microservice to get data

--- conference-app/server/config/index.js
+++ conference-app/server/config/index.js
@@ -3,16 +3,18 @@ const path = require('path');
 module.exports = {
   development: {
     sitename: 'Roux Meetups [Development]',
+    serviceRegistryUrl: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
     data: {
-      speakers: path.join(__dirname, '../data/speakers.json'),
-      feedback: path.join(__dirname, '../data/feedback.json'),
-    },
+      feedback: path.join(__dirname, '../data/feedback.json')
+    }
   },
   production: {
     sitename: 'Roux Meetups',
+    serviceRegistryUrl: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
     data: {
-      speakers: path.join(__dirname, '../data/speakers.json'),
-      feedback: path.join(__dirname, '../data/feedback.json'),
-    },
-  },
+      feedback: path.join(__dirname, '../data/feedback.json')
+    }
+  }
 };
